15069ed50737b84822761f0bf2e6f983da259bed
Paweł <Pawe@arrakis>
2018-11-28 22:12:31 +0100

    Kolejne okna i operacja dodawania nowego klienta.

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,18 +4,18 @@ import java.util.*;
 import app.model.*;
 import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.gui.Action;
-import com.googlecode.lanterna.gui.Component;
 import com.googlecode.lanterna.gui.GUIScreen;
 import com.googlecode.lanterna.gui.Window;
 import com.googlecode.lanterna.gui.component.Button;
 import com.googlecode.lanterna.gui.component.EmptySpace;
 import com.googlecode.lanterna.gui.component.Label;
+import com.googlecode.lanterna.gui.component.TextBox;
 import com.googlecode.lanterna.gui.layout.LinearLayout;
 import com.googlecode.lanterna.terminal.TerminalSize;
 
 
 public class Application {
-	
+    
     public static void w1_welcome(GUIScreen guiScreen) {
         final Window window = new Window("Witaj w aplikacji BANK");        
         window.setSoloWindow(true);
@@ -34,11 +34,10 @@ public class Application {
         window.addComponent(okButton, LinearLayout.GROWS_HORIZONTALLY);
         guiScreen.showWindow(window, GUIScreen.Position.CENTER);
     }
-    
-    public static void w2_test(GUIScreen guiScreen, List<Client> lista) {
+
+    public static void w2_klienci(GUIScreen guiScreen, List<Client> lista) {
         final Window window = new Window("BANK");
         window.setSoloWindow(true);
-        window.setWindowSizeOverride(new TerminalSize(80,10));
         Label lbl_klienci = new Label("Klienci naszego banku:");
         Button okButton = new Button("Zamknij", new Action(){
             @Override
@@ -56,8 +55,42 @@ public class Application {
         window.addComponent(new EmptySpace(), LinearLayout.GROWS_VERTICALLY);
         window.addComponent(okButton, LinearLayout.GROWS_VERTICALLY);
         
-        guiScreen.showWindow(window, GUIScreen.Position.CENTER);
+        guiScreen.showWindow(window, GUIScreen.Position.CENTER);        
+    }
+    
+    public static List<Client> w3_dodaj_klienta(GUIScreen guiScreen, List<Client> lista) {
+        final Window window = new Window("BANK");
+        List<Client> lista_klientow = lista;
         
+        
+        
+        window.setSoloWindow(true);
+        Label lbl_dodaj = new Label("Wpisz nazwę nowego klienta:");
+        TextBox txt_dodaj = new TextBox();
+        txt_dodaj.setPreferredSize(new TerminalSize(10,1));
+        Button dodajButton = new Button("Dodaj", new Action(){
+            @Override
+            public void doAction(){
+                lista_klientow.add(new Client(txt_dodaj.getText()));
+                txt_dodaj.setText("");
+            }
+        });
+        Button wyjdzButton = new Button("Wyjdz", new Action(){
+            @Override
+            public void doAction(){
+                window.close();
+            }
+        });
+        
+        
+        window.addComponent(lbl_dodaj, LinearLayout.GROWS_VERTICALLY);
+        window.addComponent(txt_dodaj, LinearLayout.GROWS_VERTICALLY);
+        window.addComponent(new EmptySpace(), LinearLayout.GROWS_VERTICALLY);
+        window.addComponent(dodajButton, LinearLayout.GROWS_VERTICALLY);
+        window.addComponent(wyjdzButton, LinearLayout.GROWS_VERTICALLY);
+        
+        guiScreen.showWindow(window, GUIScreen.Position.CENTER);
+        return lista_klientow;
     }
     
     public static void main(String [] args){
@@ -77,11 +110,10 @@ public class Application {
 		c.newAccount(new AccountSavings(300, 0.80));				
             }
 	}
-        
         w1_welcome(guiScreen);
-        w2_test(guiScreen, clients);
-
-
+        w2_klienci(guiScreen, clients);
+        clients = w3_dodaj_klienta(guiScreen, clients);
+        w2_klienci(guiScreen, clients);
 
 
 
